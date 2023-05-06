45716b181d266f40fad33a87c70b96ac9196c452
Mauran <thomas.mauran@etu.umontpellier.fr>
2023-05-06 11:44:33 +0200

    feat(chart1)

--- server.R
+++ server.R
@@ -8,21 +8,50 @@
 #
 
 library(shiny)
+library(ggplot2)
+library(dplyr)
+library(treemapify)
+library(RColorBrewer)
+
+data <- read.csv("https://raw.githubusercontent.com/thomas-mauran/Exoplanets-R-visualization/main/data.csv")
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+  
+  output$plot1 <- renderPlot({
+    # Sous tableau
+    subset_year_type <- subset(data, Discovery.Method %in% input$method)
+    # Ajout de fréquences
+    year_method_frequency <- table(subset_year_type$Discovery.Year, subset_year_type$Discovery.Method)
+    
+    # Conversion en data frame
+    year_method_frequency_df <- data.frame(Year = as.integer(rownames(year_method_frequency)),
+                                           Method = colnames(year_method_frequency),
+                                           Frequency = as.vector(year_method_frequency))
+    # Colors 
+    # Generate a rainbow color palette with 11 colors
+    col_pal <- c("strometry" = "#BEBADA",
+                 "Disk Kinematics" = "#8DD3C7",
+                 "Eclipse Timing Variations" = "#80B1D3",
+                 "Imaging" = "#FDB462",
+                 "Microlensing" = "#B3DE69",
+                 "Orbital Brightness Modulation" = "#FCCDE5",
+                 "Pulsar Timing" = "#D9D9D9",
+                 "Pulsation Timing Variations" = "#BC80BD",
+                 "Radial Velocity" = "#CCEBC5",
+                 "Transit" = "#FFED6F",
+                 "Transit Timing Variations" = "#FFFFB3")
+    
+    
+    # Gaphique
+    ggplot(year_method_frequency_df, aes(x = Year, y = Frequency, fill = Method)) +
+      geom_bar(stat = "identity") +
+      scale_fill_manual(values = col_pal) +
+      labs(title = "Nombre d'exoplanètes découvertes par année et par méthode",
+           x = "Année",
+           y = "Fréquence",
+           fill = "Méthode de découverte") +
+      scale_x_continuous(breaks = seq(1985, 2023, 5)) +
+    coord_cartesian(xlim = input$slider2)
+  })
 }
--- ui.R
+++ ui.R
@@ -9,10 +9,68 @@
 
 library(shiny)
 library(shinydashboard)
+library(dplyr)
+
+data <- read.csv("https://raw.githubusercontent.com/thomas-mauran/Exoplanets-R-visualization/main/data.csv")
+
+
+# CHART 1
+chart1 <- fluidRow(
+  box(plotOutput("plot1"), width = 12),
+  box(
+    title = "Controls",
+    splitLayout(
+      sliderInput(
+        "slider2",
+        label = h3("Discover year range"),
+        min = 1985,
+        max = 2023,
+        value = c(1985, 2023),
+        sep = "",
+        animate = TRUE
+      ),
+      div(
+        checkboxGroupInput(
+          "method",
+          label = h3("Discovery Method"),
+          choices = sort(unique(data$Discovery.Method)),
+          selected = unique(data$Discovery.Method),
+        ),
+        style = "margin: 0px 20px;"
+      ),
+      cellWidths = c("70%", "30%"),
+    ),
+    width = 12
+  ),
+)
+
+# SIDEBAR
+sidebar <- dashboardSidebar(
+  tags$style(".sidebar-menu li a {font-size: 1.2em;}"),
+  sidebarMenu(
+    menuItem("Number / year", tabName = "chart1", icon = icon("chart-column")),
+    menuItem("Discover Facility", tabName = "chart2", icon = icon("chart-simple")),
+    menuItem("Stellar mass and radius", tabName = "chart3", icon = icon("chart-column"))
+  )
+)
+
+# BODY
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "chart1",
+            chart1
+    ),
+    tabItem(tabName = "chart2",
+            h2("chart2")
+    )
+  )
+)
+
+
+# PAGE
 dashboardPage(
   skin = "purple",
   dashboardHeader(title = "Exoplanet datavis"),
-  dashboardSidebar(),
-  dashboardBody()
+  sidebar,
+  body
 )
-
